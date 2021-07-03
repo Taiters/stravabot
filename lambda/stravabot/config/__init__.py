20cc99a6f26382a95650781a562f954c507089e4
Daniel Tait <dantait91@gmail.com>
2021-07-03 17:46:19 +0100

    Fix bad if statement when loading config
    if -> elif

--- lambda/stravabot/config/__init__.py
+++ lambda/stravabot/config/__init__.py
@@ -5,7 +5,7 @@ from stravabot.config.base import *  # noqa
 env = os.environ.get("STRAVABOT_ENV", "prod")
 if env == "test":
     from stravabot.config.test import *  # noqa
-if env == "local":
+elif env == "local":
     from stravabot.config.local import *  # noqa
 else:
     from stravabot.config.prod import *  # noqa
