db4833d0894f747227d4878f9bf0cd8a79fcc851
Daniel Tait <dantait91@gmail.com>
2021-07-13 21:13:52 +0100

    Revert "Log import times"
    This reverts commit b30b8b2036991927658262b30181cba15dd41c19.

--- lambda/app.py
+++ lambda/app.py
@@ -1,22 +1,10 @@
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-logging.info("patch all import")
 from aws_xray_sdk.core import patch_all
 
-logging.info("bootstrap import")
 from stravabot.bootstrap import bootstrap
 
-logging.info("starting patch all")
 patch_all()
-logging.info("ending patch all")
-
-logging.info("calling bootstrap")
 api = bootstrap()
-logging.info("returned from bootstrap")
 
 
 def handler(event, context):
-    logging.info("handler")
     return api.handle(event, context)
--- lambda/stravabot/bootstrap.py
+++ lambda/stravabot/bootstrap.py
@@ -1,63 +1,22 @@
-import logging
-from contextlib import contextmanager
-import time
+import boto3
+from jinja2 import Environment, PackageLoader, select_autoescape
+from slack_bolt import App
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from stravabot.api import Api
+from stravabot.config import JWT_SECRET_KEY, KV_STORE_TABLE, STRAVA_EVENT_HANDLER
+from stravabot.config.base import AUTH_FLOW_TTL
+from stravabot.db import KeyValueStore
+from stravabot.handlers import StravaAuthHandler, StravaEventHandler
+from stravabot.handlers.channel import ChannelHandler
+from stravabot.services.event import EventService
+from stravabot.services.response_url import ResponseUrlService
+from stravabot.services.slack import SlackService
+from stravabot.services.strava import StravaService
+from stravabot.services.token import TokenService
+from stravabot.services.user import UserService
 
-
-@contextmanager
-def timed(name):
-    start = time.time()
-    yield
-    end = time.time()
-    logging.info(f"importing {name} took {end - start} seconds")
-
-
-with timed("boto3"):
-    import boto3
-
-with timed("jinja2"):
-    from jinja2 import Environment, PackageLoader, select_autoescape
-
-with timed("slack_bolt"):
-    from slack_bolt import App
-
-with timed("stravabot.api"):
-    from stravabot.api import Api
-
-with timed("stravabot.config"):
-    from stravabot.config import JWT_SECRET_KEY, KV_STORE_TABLE, STRAVA_EVENT_HANDLER, AUTH_FLOW_TTL
-
-with timed("stravabot.db"):
-    from stravabot.db import KeyValueStore
-
-with timed("stravabot.handlers"):
-    from stravabot.handlers import StravaAuthHandler, StravaEventHandler
-
-with timed("stravabot.handlers.channel"):
-    from stravabot.handlers.channel import ChannelHandler
-
-with timed("stravabot.services.event"):
-    from stravabot.services.event import EventService
-
-with timed("stravabot.services.response_url"):
-    from stravabot.services.response_url import ResponseUrlService
-
-with timed("stravabot.services.slack"):
-    from stravabot.services.slack import SlackService
-
-with timed("stravabot.services.strava"):
-    from stravabot.services.strava import StravaService
-
-with timed("stravabot.services.token"):
-    from stravabot.services.token import TokenService
-
-with timed("stravabot.services.user"):
-    from stravabot.services.user import UserService
 
 def bootstrap() -> Api:
-    logger.info("bootstrap starting")
     # templates = Environment(
     #     loader=PackageLoader("stravabot"),
     #     autoescape=select_autoescape(),
@@ -89,5 +48,4 @@ def bootstrap() -> Api:
     api.route("/strava/event", methods=["POST"])(event_handler.handle_webhook)
     # api.route("/strava/event", methods=["GET"])(event_handler.handle_verify)
 
-    logger.info("bootstrap ending")
     return api
