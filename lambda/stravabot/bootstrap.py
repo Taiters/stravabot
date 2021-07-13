7724da55bd43d5769b2fec1680c9594f43c3fb78
Daniel Tait <dantait91@gmail.com>
2021-07-13 20:23:36 +0100

    Only instantiate whats needed for webhook

--- lambda/stravabot/bootstrap.py
+++ lambda/stravabot/bootstrap.py
@@ -17,35 +17,35 @@ from stravabot.services.user import UserService
 
 
 def bootstrap() -> Api:
-    templates = Environment(
-        loader=PackageLoader("stravabot"),
-        autoescape=select_autoescape(),
-    )
-    app = App(process_before_response=True)
-    api = Api(app)
-    dynamodb = boto3.resource("dynamodb")
-    store = KeyValueStore(dynamodb.Table(KV_STORE_TABLE))
+    # templates = Environment(
+    #     loader=PackageLoader("stravabot"),
+    #     autoescape=select_autoescape(),
+    # )
+    # app = App(process_before_response=True)
+    api = Api(None)
+    # dynamodb = boto3.resource("dynamodb")
+    # store = KeyValueStore(dynamodb.Table(KV_STORE_TABLE))
 
-    tokens = TokenService(JWT_SECRET_KEY)
-    users = UserService(store)
-    strava = StravaService(users)
-    response_urls = ResponseUrlService(store, tokens)
+    # tokens = TokenService(JWT_SECRET_KEY)
+    # users = UserService(store)
+    # strava = StravaService(users)
+    # response_urls = ResponseUrlService(store, tokens)
     events = EventService(STRAVA_EVENT_HANDLER, boto3.client("lambda"))
-    slack = SlackService(app)
+    # slack = SlackService(app)
 
-    channel_handler = ChannelHandler(slack)
-    auth_handler = StravaAuthHandler(templates, users, strava, tokens, response_urls, AUTH_FLOW_TTL)
-    with api.command("/creep") as creep:
-        creep.on("connect", "Connect to your Strava account")(auth_handler.handle_connect_command)
-        creep.on("disconnect", "Disconnect your Strava account")(auth_handler.handle_disconnect_command)
-        creep.on("kick", "Remove from the current channel")(channel_handler.handle_kick_command)
+    # channel_handler = ChannelHandler(slack)
+    # auth_handler = StravaAuthHandler(templates, users, strava, tokens, response_urls, AUTH_FLOW_TTL)
+    # with api.command("/creep") as creep:
+    #     creep.on("connect", "Connect to your Strava account")(auth_handler.handle_connect_command)
+    #     creep.on("disconnect", "Disconnect your Strava account")(auth_handler.handle_disconnect_command)
+    #     creep.on("kick", "Remove from the current channel")(channel_handler.handle_kick_command)
 
-    api.slack.action("authenticate_clicked")(auth_handler.handle_authenticate_action)  # type: ignore
-    api.route("/strava/auth", methods=["GET"])(auth_handler.handle_strava_callback)
-    api.route("/strava/auth", methods=["POST"])(auth_handler.handle_strava_connect)
+    # api.slack.action("authenticate_clicked")(auth_handler.handle_authenticate_action)  # type: ignore
+    # api.route("/strava/auth", methods=["GET"])(auth_handler.handle_strava_callback)
+    # api.route("/strava/auth", methods=["POST"])(auth_handler.handle_strava_connect)
 
     event_handler = StravaEventHandler(events)
     api.route("/strava/event", methods=["POST"])(event_handler.handle_webhook)
-    api.route("/strava/event", methods=["GET"])(event_handler.handle_verify)
+    # api.route("/strava/event", methods=["GET"])(event_handler.handle_verify)
 
     return api
